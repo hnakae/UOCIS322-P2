b29507a6fbeb5b17178260471d09d5d5d0cd8b38
Hiro Nakae <nakae27@testium.cs.uoregon.edu>
2021-04-25 11:04:39 -0700

    test

--- web/app.py
+++ web/app.py
@@ -1,14 +1,26 @@
 """
-John Doe's Flask API.
+Hiro Nakae's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, render_template
+import os.path
+from os import path
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+def hello(message="200/OK"):
+    return render_template('index.html', msg=message)
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def error_403(e):
+    return render_template('403.html'), 403
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, 
+            host='0.0.0.0', 
+            port=5000)
